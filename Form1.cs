230ee532b40152305bbd7615c743169f948053e7
james0427 <jamiller0427@my.mwsu.edu>
2015-11-29 17:36:59 -0600

    changed the nagivation function

--- Form1.cs
+++ Form1.cs
@@ -25,10 +25,15 @@ namespace Simple_Web_Browser
         {
             this.Close();
         }
-
+        /// <summary>
+        /// This function is called when the about section is selected.
+        /// Just a basic about section of the code which can be edited to your liking.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void aboutToolStripMenuItem_Click(object sender, EventArgs e)
         {
-            MessageBox.Show("This program was made by James Miller");
+            MessageBox.Show("Place about me here");
         }
         /// <summary>
         /// On click the web control displayes the page requested, by URL
@@ -41,6 +46,8 @@ namespace Simple_Web_Browser
         }
         /// <summary>
         /// Core navigation function
+        /// Allows for easy recoding of the navigation functions without repeating mass amounts of code.
+        /// 
         /// </summary>
         private void Navigate()
         {
@@ -48,8 +55,15 @@ namespace Simple_Web_Browser
             button1.Enabled = false;
             textBox1.Enabled = false;
             webBrowser1.Navigate(textBox1.Text);
-
         }
+
+        /// <summary>
+        /// Checks to see if the page has completely loaded.
+        /// Once it is loaded it will unlock the nagivation bar and button
+        /// Allowing you to continue surfing
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
 
         private void webBrowser1_DocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e)
         {
@@ -58,11 +72,20 @@ namespace Simple_Web_Browser
             textBox1.Text = webBrowser1.Url.ToString();
         }
 
+        /// <summary>
+        /// Textbox - Unused for code
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void textBox1_TextChanged(object sender, EventArgs e)
         {
 
         }
-
+        /// <summary>
+        /// Triggered event once the user has clicked the button.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
         {
             if (e.KeyChar == (char)ConsoleKey.Enter)
@@ -71,6 +94,15 @@ namespace Simple_Web_Browser
             }
         }
 
+        /// <summary>
+        /// Status bar located at the bottom of the page
+        /// Gets updates based on the percent of the page that has been loaded in bytes
+        /// 
+        /// Sets the maximum side to the total percent of the bytes
+        /// (other wise the program will get an overflow error)
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void webBrowser1_ProgressChanged(object sender, WebBrowserProgressChangedEventArgs e)
         {
             if (e.MaximumProgress > 0 && e.CurrentProgress > 0)
