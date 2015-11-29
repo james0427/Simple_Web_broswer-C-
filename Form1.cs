457ad44d495f8c40aef709b56841c9de53dc1c4a
james0427 <jamiller0427@my.mwsu.edu>
2015-11-29 17:31:27 -0600

    Basic web browser, starter code, C#

---
+++ Form1.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Simple_Web_Browser
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        /// <summary>
+        /// This function is called when the exit menu is selected
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void aboutToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("This program was made by James Miller");
+        }
+        /// <summary>
+        /// On click the web control displayes the page requested, by URL
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Navigate();
+        }
+        /// <summary>
+        /// Core navigation function
+        /// </summary>
+        private void Navigate()
+        {
+            toolStripStatusLabel1.Text = "Page loading";
+            button1.Enabled = false;
+            textBox1.Enabled = false;
+            webBrowser1.Navigate(textBox1.Text);
+
+        }
+
+        private void webBrowser1_DocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e)
+        {
+            button1.Enabled = true;
+            textBox1.Enabled = true;
+            textBox1.Text = webBrowser1.Url.ToString();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == (char)ConsoleKey.Enter)
+            {
+                Navigate();
+            }
+        }
+
+        private void webBrowser1_ProgressChanged(object sender, WebBrowserProgressChangedEventArgs e)
+        {
+            if (e.MaximumProgress > 0 && e.CurrentProgress > 0)
+            {
+                toolStripProgressBar1.ProgressBar.Maximum = (int)((e.CurrentProgress *100) / e.MaximumProgress) + 1;
+                toolStripProgressBar1.ProgressBar.Value = (int)((e.CurrentProgress *100) / e.MaximumProgress);
+            }
+
+            toolStripStatusLabel1.Text = "Loading completed";
+        }
+
+        private void toolStripProgressBar1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void toolStripStatusLabel1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
